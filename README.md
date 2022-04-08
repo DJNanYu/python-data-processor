d1d005907905bea774d2b4a881acde38b9829b9e
DJNanYu <2986945687@qq.com>
2022-04-08 10:00:00 -0400

    feat: initial draft of python-data-processor

---
+++ README.md
@@ -0,0 +1,5 @@
+# python-data-processor
+
+Android Security Bulletin Scraper. 安卓安全公告爬虫
+
+Initial development of python-data-processor.
---
+++ main.py
@@ -0,0 +1,10 @@
+// Draft version for python-data-processor
+
+
+import requests
+from bs4 import BeautifulSoup
+import json
+import time
+
+class AndroidSecScraper:
+   ...
